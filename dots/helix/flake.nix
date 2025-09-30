4e48901299393509671c5b3270e32ab2db98f65a
aschoettler <18386218+aschoettler@users.noreply.github.com>
2025-09-29 17:07:42 -0700

    fix: converted nixos module to home module

--- dots/helix/flake.nix
+++ dots/helix/flake.nix
@@ -1,12 +1,10 @@
 {
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-
   outputs =
     { self }:
     {
       ### This nixos module imports all the themes in the ./helix-themes folder. Home manager will symlink them under ~/.config/helix/themes/.
       ### Once you've imported the module, set `programs.helix.theme = "onedark-vibrant"`
-      nixosModules.helix-themes =
+      homeModules.helix-themes =
         { lib, ... }:
         let
           themeDir = ./helix-themes;
